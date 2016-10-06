56aff7bdae83ab7caa1ab2fae12e1aceb18a220f
Gibberish314 <a.moosen@gmail.com>
2016-10-06 14:40:27 -0400

    Actually, start with just a blank App Component and add the router to that.

---
+++ app/album-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Album } from './album.model';
+
+@Component({
+  selector: 'albums',
+  template: `
+    <div class="album" *ngFor="let album of albums">
+      <p>{{album.title}}</p>
+      <p>By {{album.artist}}</p>
+      <p>Released in {{album.released}}</p>
+    </div>
+  `
+})
+
+export class AlbumListComponent {
+  @Input() albumList: Album[];
+}
+
+
+// albums: Album[] = [
+//     new Album("Pulse", "Pink Floyd", 1995),
+//     new Album("Funhouse", "The Stooges", 1970),
+//     new Album("Twilight of the Thunder God", "Amon Amarth", 2008),
+//     new Album("Dilate", "Ani DiFranco", 1996),
+//     new Album("Chopin - Complete Nocturnes", "Brigitte Engerer", 2015),
+//     new Album("Axis Bold As Love", "The Jimi Hendrix Experience", 1967)
+//   ]
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,15 @@
 import { Component } from '@angular/core';
-import { Album } from './album.model';
 
 @Component({
   selector: 'my-app',
   template: `
   <div class="container">
-    <h1>My Music Review Site</h1>
-    <div class="album" *ngFor="let album of albums">
-      <p>{{album.title}}</p>
-      <p>By {{album.artist}}</p>
-      <p>Released in {{album.released}}</p>
-    </div>
+    <h1>Cameron's Music Site</h1>
+
   </div>
   `
 })
 
 export class AppComponent { 
-  albums: Album[] = [
-    new Album("Pulse", "Pink Floyd", 1995),
-    new Album("Funhouse", "The Stooges", 1970),
-    new Album("Twilight of the Thunder God", "Amon Amarth", 2008),
-    new Album("Dilate", "Ani DiFranco", 1996),
-    new Album("Chopin - Complete Nocturnes", "Brigitte Engerer", 2015),
-    new Album("Axis Bold As Love", "The Jimi Hendrix Experience", 1967)
-  ]
+  
 }
