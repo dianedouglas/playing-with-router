50804744458acd922e914a518f6291c2dd714f8e
Gibberish314 <a.moosen@gmail.com>
2016-10-06 20:10:30 -0400

    make a basic component load with the router.

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,33 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-
 @Component({
   moduleId: module.id,
   selector: 'my-dashboard',
-  templateUrl: 'dashboard.component.html',
-  styleUrls: [ 'dashboard.component.css' ]
+  template: `
+    <p>sup</p>
+  `
 })
 export class DashboardComponent implements OnInit {
 
-  heroes: Hero[] = [];
+  constructor(private router: Router) {}
 
-  constructor(
-    private router: Router,
-    private heroService: HeroService) {
-  }
-
-  ngOnInit(): void {
-    this.heroService.getHeroes()
-      .then(heroes => this.heroes = heroes.slice(1, 5));
-  }
-
-  gotoDetail(hero: Hero): void {
-    let link = ['/detail', hero.id];
-    this.router.navigate(link);
-  }
+  ngOnInit(): void { }
 }
 
 
