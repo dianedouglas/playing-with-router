e187d69865b109773d01173b77d91958a838dc14
Gibberish314 <a.moosen@gmail.com>
2016-10-07 12:05:39 -0400

    Add mock-albums file to hold the data as a database standin, add album service file to fetch the data and make it available to all components, refactor album-list component to get the data fromthe service.

---
+++ app/album.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Album } from './album.model';
+import { ALBUMS } from './mock-albums';
+
+//same structure as a component. Imports, followed by a decorator with the @ symbol, then an exported class declaration.
+//note the dash-case convention with .typeofthing.ts for the file name, and the name of the file is translated to upper camel case for the class declaration here.
+
+@Injectable()
+export class AlbumService {
+  getAlbums = function() {
+    return ALBUMS;
+  }
+}
---
+++ app/mock-albums.ts
@@ -0,0 +1,10 @@
+import { Album } from './album.model';
+
+export const ALBUMS: Album[] = [
+  new Album("Pulse", "Pink Floyd", 1995, 1),
+  new Album("Funhouse", "The Stooges", 1970, 2),
+  new Album("Twilight of the Thunder God", "Amon Amarth", 2008, 3),
+  new Album("Dilate", "Ani DiFranco", 1996, 4),
+  new Album("Chopin - Complete Nocturnes", "Brigitte Engerer", 2015, 5),
+  new Album("Axis Bold As Love", "The Jimi Hendrix Experience", 1967, 6)
+];
