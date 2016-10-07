02a788a0a9cfa91490f2dbbacb03f0e7aed52be9
Gibberish314 <a.moosen@gmail.com>
2016-10-06 20:04:01 -0400

    reverse engineer tour of heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,13 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <div class="container">
-    <h1>Cameron's Music Site</h1>
-
-  </div>
+    <h1>Cameron's Music</h1>
+    <router-outlet></router-outlet>
   `
 })
+export class AppComponent {
 
-export class AppComponent { 
-  
 }
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
